8b1792c6668ab6bad94442cb46285a96ad99cc03
jbolaty <jeanmarcbolaty@yahoo.fr>
2019-04-03 11:36:42 +0200

    installation de firebase et liaison avec notre application

--- libraryManage/src/app/app.component.ts
+++ libraryManage/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'libraryManage';
+
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyDmHniykn6j_RgJzMCSCkVmgf18YbJwF9c",
+      authDomain: "librarymanage-d8a9a.firebaseapp.com",
+      databaseURL: "https://librarymanage-d8a9a.firebaseio.com",
+      projectId: "librarymanage-d8a9a",
+      storageBucket: "librarymanage-d8a9a.appspot.com",
+      messagingSenderId: "1055885159066"
+    };
+    firebase.initializeApp(config);
+  }
 }
