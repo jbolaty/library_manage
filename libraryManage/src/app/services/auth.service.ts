520143e826b95318766faa2cc6e67a9f1c7de041
jbolaty <jeanmarcbolaty@yahoo.fr>
2019-04-03 12:02:55 +0200

    édition de 3 méthodes dans auth.service.ts permettant de: créer un utilisateur, la connexion d'un utilisateur, la deconnexion de l'utilisateur

--- libraryManage/src/app/services/auth.service.ts
+++ libraryManage/src/app/services/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { reject } from 'q';
+
+
+// Ici on veut créer 3 méthodes
+// - une pour créer un nouvel utilisateur 
+// - une pour premettre à un utilisateur de se connecter
+// - une pour permettre à l'utilisateur de se connecter 
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +14,44 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+  
+  // Créer un nouvel utilisateur (asynchrone)
+  createNewUser(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(
+          () => {
+            resolve();
+          },
+          (error)=>{
+            reject(error)
+          }
+        );
+      }
+    );
+  }
+
+  // Conection de l'utilisateur (asynchrone)
+  singnInUser(email: string, password: string){
+    return new Promise(
+      (resolve, reject)=>{
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error)
+          }
+        )
+      }
+    )
+  }
+
+  // déconnection de l'utilisateur 
+  sinOutUser(){
+    firebase.auth().signOut();
+  }
+
 }
