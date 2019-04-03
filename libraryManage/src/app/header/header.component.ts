dca26efc1f1c325f6f90844ce14d3910710789a8
jbolaty <jeanmarcbolaty@yahoo.fr>
2019-04-03 15:55:49 +0200

    Affichage du boutton <déconnexion> quand l'utilisateur est déjà connecté, et <connexion et créer un compte> quand l'utilisateur ne l'est pas encore

--- libraryManage/src/app/header/header.component.ts
+++ libraryManage/src/app/header/header.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isAuth: boolean;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    firebase.auth().onAuthStateChanged(
+      // Si firebase retourne un user alors this.isAuth = true 
+      (user)=>{
+        if(user){
+          this.isAuth = true;
+        } else {
+          this.isAuth = false;
+      }
+    });
+  }
+
+  onSignOut(){
+    this.authService.sinOutUser();
   }
 
 }
