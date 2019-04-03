db573eef7d2e63ec3fc815b74221915ade8d5213
jbolaty <jeanmarcbolaty@yahoo.fr>
2019-04-03 16:50:41 +0200

    Implémentation du Guard, pour que les pages ne soient accessibles que si l'utilisateur est connecté

--- libraryManage/src/app/services/auth-guard.service.ts
+++ libraryManage/src/app/services/auth-guard.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase';
+
+// authGuard est Asynchrone 
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
 
-  constructor() { }
+
+export class AuthGuardService implements CanActivate {
+
+  constructor(private router: Router) { }
+
+canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    return new Promise (
+        (resolve, reject)=>{
+          firebase.auth().onAuthStateChanged(
+            (user)=>{
+              if(user){
+                resolve(true);
+              } else {
+                this.router.navigate(['/auth','signin']);
+                resolve(false); // pour dire qu'il n'a pas le droit d'accéder
+              }
+            }
+          );
+        }
+      );
+    }
 }
