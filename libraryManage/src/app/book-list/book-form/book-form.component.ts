6c5b04c4578727bcc694d0ab4bea161e9ed8a132
jbolaty <jeanmarcbolaty@yahoo.fr>
2019-04-04 11:42:09 +0200

    Edition d'un formulaire pour enregistrer un nouveau livre (book-form.component), un component pour afficher un seul livre (single-book.component), et un component pour voir la liste de tous les livres enregistrés (book-list.component)

--- libraryManage/src/app/book-list/book-form/book-form.component.ts
+++ libraryManage/src/app/book-list/book-form/book-form.component.ts
@@ -1,15 +1,48 @@
+// Formulaire pour remplir les informations du livres
+
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BooksService } from 'src/app/services/books.service';
+import { Router } from '@angular/router';
+import { Book } from 'src/app/models.ts/book.model';
 
 @Component({
   selector: 'app-book-form',
   templateUrl: './book-form.component.html',
   styleUrls: ['./book-form.component.scss']
 })
+
 export class BookFormComponent implements OnInit {
 
-  constructor() { }
+  bookForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private booksService: BooksService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(){
+    this.bookForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required]
+    });
+  }
+
+
+    // OnSaveBook va prendre les informations du formulaire, en créer un nouveau livre,
+    // l'ajouter etnavigue vers /books 
+  onSaveBook(){
+    const title = this.bookForm.get('title').value;
+    const author = this.bookForm.get('author').value;
+
+    const newBook = new Book(title, author);
+    this.booksService.createNewBook(newBook);
+
+    this.router.navigate(['/books']);
+
   }
 
 }
--- libraryManage/src/app/book-list/single-book/single-book.component.ts
+++ libraryManage/src/app/book-list/single-book/single-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models.ts/book.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BooksService } from 'src/app/services/books.service';
 
 @Component({
   selector: 'app-single-book',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleBookComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
+
+  constructor(private route: ActivatedRoute,
+              private booksService: BooksService,
+              private router: Router) { }
 
   ngOnInit() {
+     // on créer un book vide temporaire en attendant que le resultat de la recherche soit disponible
+     // une fois que le livre est récupéré depuis le serveur il remplace le book temporaire vide 
+    this.book = new Book('', '');
+    const id = this.route.snapshot.params['id'];
+    this.booksService.getSingleBook(+id).then(
+      (book: Book) => {
+        this.book = book;
+      }
+    );
+  }
+
+    // méthode pour faire un retour en arrière 
+  onBack(){
+    this.router.navigate(['/books']);
   }
 
 }
